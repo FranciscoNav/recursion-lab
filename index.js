49ab6802ee60c7ae50a1e720e79b49a4ad3a67be
Francisco <fnaveira11@gmail.com>
2021-09-15 13:26:55 -0400

    wrote first recusion

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Code your solution here!
 
+function printString(param) {
+    console.log(param[0]);
+  
+    if(param.length > 1){
+      let mySubstring;
+  
+      mySubstring= param.substring(1, param.length)
+  
+      printString(mySubstring)
+    }else{
+      return true
+    }
+  }
